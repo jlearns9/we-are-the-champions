6ed6e7434097d93c2af812b0382c3abf460136b6
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-09-13 00:47:03 -0500

    uPdate

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ const endorsementListInDB = ref(database, "endorsementList")
 const inputFieldEl = document.getElementById("input-field")
 const publishButtonEl = document.getElementById("publish-button")
 const endorsementListEl = document.getElementById("endorsement-list")
+const toFieldEl = document.getElementById("to-field")
+const fromFieldEl = document.getElementById("from-field")
 
-// Meat & Potatoes
+// Publish Button
 publishButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    push(endorsementListInDB, inputValue)
+    let inputValue = inputFieldEl.value.trim()
+    let toValue = toFieldEl.value.trim()
+    let fromValue = fromFieldEl.value.trim()
+    if (inputValue === "" || toValue === "" || fromValue === "") {
+        alert("Error: All fields must be filed")
+        return
+    }
+    let endorsement = {
+        message: inputValue,
+        to: toValue,
+        from: fromValue
+    }
+    push(endorsementListInDB, endorsement)
     clearInputFieldEl()
 })
 
@@ -31,8 +44,6 @@ onValue(endorsementListInDB, function(snapshot) {
         clearEndorsementListEl()
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
             appendItemToEndorsementListEl(currentItem)
         }
     } else {
@@ -44,6 +55,8 @@ onValue(endorsementListInDB, function(snapshot) {
 // Functions
 function clearInputFieldEl() {
     inputFieldEl.value = ""
+    toFieldEl.value = ""
+    fromFieldEl.value = ""
 }
 
 function clearEndorsementListEl() {
@@ -51,13 +64,25 @@ function clearEndorsementListEl() {
 }
 
 function appendItemToEndorsementListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    let newEl = document.createElement("li")
-    newEl.textContent = itemValue
-    newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `endorsementList/${itemID}`)
-        remove(exactLocationOfItemInDB)
-    })
-    endorsementListEl.append(newEl)
+    let endorsement = item[1]
+
+    let newEl = document.createElement("li")  // Creates the list Element
+
+    let fromEl = document.createElement("div")  // Creates From: Element
+    fromEl.textContent = `From ${endorsement.from}`
+    fromEl.classList.add('bold-text')
+
+    let messageEl = document.createElement("div")  // Creates Message Element
+    messageEl.textContent = endorsement.message
+
+    let toEl = document.createElement("div")  // Creates To: Element
+    toEl.textContent = `To ${endorsement.to}`
+    toEl.classList.add('bold-text')
+
+    newEl.append(fromEl, messageEl, toEl)
+    if (endorsementListEl.firstChild) {
+        endorsementListEl.insertBefore(newEl, endorsementListEl.firstChild);
+    } else {
+        endorsementListEl.append(newEl);
+    }
 }
